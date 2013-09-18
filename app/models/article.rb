602433b021495c726bd7a89d50fd2f2100d56a1a
runup <645112281@qq.com>
2013-09-18 22:48:13 +0800

    ccccc

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  
+  attr_accessible :content, :tag, :title
+  validates :user_id, presence: true
+  default_scope order: 'articles.created_at DESC'
+  paginates_per 50
+
+  
+end
